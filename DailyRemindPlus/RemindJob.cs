490a0de134d6f38824e2df6da0c13742bfb1cd5e
Petter <mic277203@126.com>
2018-05-03 10:29:28 +0800

    add project

---
+++ DailyRemindPlus/RemindJob.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace DailyRemindPlus
+{
+    /// <summary>
+    /// 日报提醒
+    /// </summary>
+    public class RemindJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            var loginResult = EIPSys.Login(new EIPSys.LoginModel()
+            {
+                UserName = "lph",
+                Password = "lph"
+            });
+
+            if (loginResult)
+            {
+                EIPSys.Check();
+            }
+        }
+    }
+}
---
+++ DailyRemindPlus/Service1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace DailyRemindPlus
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        private static IScheduler _scheduler;
+        protected override void OnStart(string[] args)
+        {
+            _scheduler = StdSchedulerFactory.GetDefaultScheduler();
+
+            IJobDetail jobDailyRemind = JobBuilder.Create<RemindJob>()
+               .WithIdentity("jobDailyRemind", "Remind")
+               .Build();
+
+            ITrigger tgDailyRemind = TriggerBuilder.Create()
+                                    .WithIdentity("tgDailyRemind", "Remind")
+                                    .StartNow()
+                                    .WithCronSchedule("0 0 18 * * ?")
+                                    .Build();
+
+            _scheduler.ScheduleJob(jobDailyRemind, tgDailyRemind);
+
+            _scheduler.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
